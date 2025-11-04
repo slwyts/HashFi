5c051e0ad68b8a40d9ca66dd71702629541255e3
slwyts <slwyts@foxmail.com>
2025-11-04 21:47:48 +0800

    fix: 优化pages兼容性

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 // Lazy load components for better performance
 const Staking = () => import('./views/Staking.vue');
@@ -40,7 +40,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
